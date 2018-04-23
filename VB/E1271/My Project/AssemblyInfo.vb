f04d356c920ce92eeca0cc349a2f7b258a2005d8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/E1271/My Project/AssemblyInfo.vb
@@ -0,0 +1,52 @@
+﻿' Developer Express Code Central Example:
+' Multiple selection using checkbox (web style)
+' 
+' The following example shows how to implement multiple selection in the web style
+' (via check boxes). The XtraGrid does not have this functionality built-in. The
+' GridCheckMarksSelection class allows you to implement it. End-users can
+' select/unselect rows, group rows or select/unselect all rows by clicking on the
+' column header. Changing check box value does not start row editing. This example
+' is based on the http://www.devexpress.com/scid=A371 article.
+' 
+' See
+' Also:
+' http://www.devexpress.com/scid=E990
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E1271
+
+
+Imports Microsoft.VisualBasic
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("E1271")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("E1271")>
+<Assembly: AssemblyCopyright("Copyright ©  2009")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c3e08f9f-441f-4fc9-b043-0ffd9632b658")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
